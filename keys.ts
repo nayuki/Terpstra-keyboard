c90a03ff92d8e0aea23767fd4a80d9a1f0e58057
Project Nayuki <me@nayuki.io>
2024-06-10 02:56:11 +0000

    Reordered and grouped color-handling functions.

--- keys.ts
+++ keys.ts
@@ -65,6 +65,285 @@ class Rgb8Color {
 }
 
 
+function rgb2hsv(rgb: Rgb8Color): {h:number, s:number, v:number} {
+	var rr: number, gg: number, bb: number,
+		r: number = rgb.red / 255,
+		g: number = rgb.green / 255,
+		b: number = rgb.blue / 255,
+		h: number, s: number,
+		v: number = Math.max(r, g, b),
+		diff: number = v - Math.min(r, g, b),
+		diffc = function(c: number): number {
+			return (v - c) / 6 / diff + 1 / 2;
+		};
+	
+	if (diff == 0)
+		h = s = 0;
+	else {
+		s = diff / v;
+		rr = diffc(r);
+		gg = diffc(g);
+		bb = diffc(b);
+		
+		if (r === v)
+			h = bb - gg;
+		else if (g === v)
+			h = (1 / 3) + rr - bb;
+		else if (b === v)
+			h = (2 / 3) + gg - rr;
+		else
+			throw new Error("Unreachable");
+		if (h < 0)
+			h += 1;
+		else if (h > 1)
+			h -= 1;
+	}
+	return {
+		h: Math.round(h * 360),
+		s: Math.round(s * 100),
+		v: Math.round(v * 100),
+	};
+}
+
+
+function HSVtoRGB(h: number, s: number, v: number): Rgb8Color {
+	var r: number, g: number, b: number, i: number, f: number, p: number, q: number, t: number;
+	i = Math.floor(h * 6);
+	f = h * 6 - i;
+	p = v * (1 - s);
+	q = v * (1 - f * s);
+	t = v * (1 - (1 - f) * s);
+	switch (i % 6) {
+		case 0:
+			r = v; g = t; b = p;
+			break;
+		case 1:
+			r = q; g = v; b = p;
+			break;
+		case 2:
+			r = p; g = v; b = t;
+			break;
+		case 3:
+			r = p; g = q; b = v;
+			break;
+		case 4:
+			r = t; g = p; b = v;
+			break;
+		case 5:
+			r = v; g = p; b = q;
+			break;
+		default:
+			throw new Error("Unreachable");
+	}
+	
+	return new Rgb8Color(Math.floor(r * 255), Math.floor(g * 255), Math.floor(b * 255));
+}
+
+
+function getContrastYIQ({red, green, blue}: Rgb8Color): string {
+	return (red * 299 + green * 587 + blue * 114) / 1000 >= 128 ? "black" : "white";
+}
+
+
+function nameToHex(colour: string): string {
+	var colours: {[index:string]: string} = {
+		"aliceblue": "#f0f8ff",
+		"antiquewhite": "#faebd7",
+		"aqua": "#00ffff",
+		"aquamarine": "#7fffd4",
+		"azure": "#f0ffff",
+		"beige": "#f5f5dc",
+		"bisque": "#ffe4c4",
+		"black": "#000000",
+		"blanchedalmond": "#ffebcd",
+		"blue": "#0000ff",
+		"blueviolet": "#8a2be2",
+		"brown": "#a52a2a",
+		"burlywood": "#deb887",
+		"cadetblue": "#5f9ea0",
+		"chartreuse": "#7fff00",
+		"chocolate": "#d2691e",
+		"coral": "#ff7f50",
+		"cornflowerblue": "#6495ed",
+		"cornsilk": "#fff8dc",
+		"crimson": "#dc143c",
+		"cyan": "#00ffff",
+		"darkblue": "#00008b",
+		"darkcyan": "#008b8b",
+		"darkgoldenrod": "#b8860b",
+		"darkgray": "#a9a9a9",
+		"darkgreen": "#006400",
+		"darkkhaki": "#bdb76b",
+		"darkmagenta": "#8b008b",
+		"darkolivegreen": "#556b2f",
+		"darkorange": "#ff8c00",
+		"darkorchid": "#9932cc",
+		"darkred": "#8b0000",
+		"darksalmon": "#e9967a",
+		"darkseagreen": "#8fbc8f",
+		"darkslateblue": "#483d8b",
+		"darkslategray": "#2f4f4f",
+		"darkturquoise": "#00ced1",
+		"darkviolet": "#9400d3",
+		"deeppink": "#ff1493",
+		"deepskyblue": "#00bfff",
+		"dimgray": "#696969",
+		"dodgerblue": "#1e90ff",
+		"firebrick": "#b22222",
+		"floralwhite": "#fffaf0",
+		"forestgreen": "#228b22",
+		"fuchsia": "#ff00ff",
+		"gainsboro": "#dcdcdc",
+		"ghostwhite": "#f8f8ff",
+		"gold": "#ffd700",
+		"goldenrod": "#daa520",
+		"gray": "#808080",
+		"green": "#008000",
+		"greenyellow": "#adff2f",
+		"honeydew": "#f0fff0",
+		"hotpink": "#ff69b4",
+		"indianred": "#cd5c5c",
+		"indigo": "#4b0082",
+		"ivory": "#fffff0",
+		"khaki": "#f0e68c",
+		"lavender": "#e6e6fa",
+		"lavenderblush": "#fff0f5",
+		"lawngreen": "#7cfc00",
+		"lemonchiffon": "#fffacd",
+		"lightblue": "#add8e6",
+		"lightcoral": "#f08080",
+		"lightcyan": "#e0ffff",
+		"lightgoldenrodyellow": "#fafad2",
+		"lightgrey": "#d3d3d3",
+		"lightgreen": "#90ee90",
+		"lightpink": "#ffb6c1",
+		"lightsalmon": "#ffa07a",
+		"lightseagreen": "#20b2aa",
+		"lightskyblue": "#87cefa",
+		"lightslategray": "#778899",
+		"lightsteelblue": "#b0c4de",
+		"lightyellow": "#ffffe0",
+		"lime": "#00ff00",
+		"limegreen": "#32cd32",
+		"linen": "#faf0e6",
+		"magenta": "#ff00ff",
+		"maroon": "#800000",
+		"mediumaquamarine": "#66cdaa",
+		"mediumblue": "#0000cd",
+		"mediumorchid": "#ba55d3",
+		"mediumpurple": "#9370d8",
+		"mediumseagreen": "#3cb371",
+		"mediumslateblue": "#7b68ee",
+		"mediumspringgreen": "#00fa9a",
+		"mediumturquoise": "#48d1cc",
+		"mediumvioletred": "#c71585",
+		"midnightblue": "#191970",
+		"mintcream": "#f5fffa",
+		"mistyrose": "#ffe4e1",
+		"moccasin": "#ffe4b5",
+		"navajowhite": "#ffdead",
+		"navy": "#000080",
+		"oldlace": "#fdf5e6",
+		"olive": "#808000",
+		"olivedrab": "#6b8e23",
+		"orange": "#ffa500",
+		"orangered": "#ff4500",
+		"orchid": "#da70d6",
+		"palegoldenrod": "#eee8aa",
+		"palegreen": "#98fb98",
+		"paleturquoise": "#afeeee",
+		"palevioletred": "#d87093",
+		"papayawhip": "#ffefd5",
+		"peachpuff": "#ffdab9",
+		"peru": "#cd853f",
+		"pink": "#ffc0cb",
+		"plum": "#dda0dd",
+		"powderblue": "#b0e0e6",
+		"purple": "#800080",
+		"red": "#ff0000",
+		"rosybrown": "#bc8f8f",
+		"royalblue": "#4169e1",
+		"saddlebrown": "#8b4513",
+		"salmon": "#fa8072",
+		"sandybrown": "#f4a460",
+		"seagreen": "#2e8b57",
+		"seashell": "#fff5ee",
+		"sienna": "#a0522d",
+		"silver": "#c0c0c0",
+		"skyblue": "#87ceeb",
+		"slateblue": "#6a5acd",
+		"slategray": "#708090",
+		"snow": "#fffafa",
+		"springgreen": "#00ff7f",
+		"steelblue": "#4682b4",
+		"tan": "#d2b48c",
+		"teal": "#008080",
+		"thistle": "#d8bfd8",
+		"tomato": "#ff6347",
+		"turquoise": "#40e0d0",
+		"violet": "#ee82ee",
+		"wheat": "#f5deb3",
+		"white": "#ffffff",
+		"whitesmoke": "#f5f5f5",
+		"yellow": "#ffff00",
+		"yellowgreen": "#9acd32",
+	};
+
+	if (typeof colours[colour.toLowerCase()] != "undefined")
+		return colours[colour.toLowerCase()];
+	else if (colour.indexOf("#") == 0)
+		return colour;
+	else if (colour.length == 6 && colour.indexOf("#") == -1)
+		return "#" + colour;
+	
+	return "#EDEDE4"; //default button color!
+}
+
+
+function centsToColor(cents: number, pressed: boolean): string {
+	if (settings.spectrum_colors) {
+		var fcolor: Rgb8Color = Rgb8Color.fromCssHex("#" + settings.fundamental_color);
+		let fcolor1 = rgb2hsv(fcolor);
+		
+		var h: number = fcolor1.h / 360;
+		var s: number = fcolor1.s / 100;
+		var v: number = fcolor1.v / 100;
+		//var h = 145 / 360; // green
+		var reduced: number = (cents / 1200) % 1;
+		if (reduced < 0)
+			reduced += 1;
+		h = (reduced + h) % 1;
+		
+		v = pressed ? v - (v / 2) : v;
+		
+		//setup text color
+		current_text_color = HSVtoRGB(h, s, v);
+		return current_text_color.toCssRgb();
+		
+	} else {
+		let colorStr: string;
+		if (typeof(notUndefined(settings.keycolors)[global_pressed_interval]) === "undefined")
+			colorStr = "#EDEDE4";
+		else
+			colorStr = notUndefined(settings.keycolors)[global_pressed_interval];
+		
+		//convert color name to hex
+		current_text_color = Rgb8Color.fromCssHex(nameToHex(colorStr));
+		
+		//convert the hex to rgb
+		let {red, green, blue} = current_text_color;
+		
+		//darken for pressed key
+		if (pressed) {
+			red -= 90;
+			green -= 90;
+		}
+		
+		return new Rgb8Color(red, green, blue).toCssRgb();
+	}
+}
+
+
 
 const keyCodeToCoords_mac: {[index:string]: Point} = {
 	27: new Point(-5, -3), // esc
@@ -1090,49 +1369,6 @@ function drawHex(p: Point, c: string): void { /* Point, color */
 	context.restore();
 }
 
-function centsToColor(cents: number, pressed: boolean): string {
-	if (settings.spectrum_colors) {
-		var fcolor: Rgb8Color = Rgb8Color.fromCssHex("#" + settings.fundamental_color);
-		let fcolor1 = rgb2hsv(fcolor);
-		
-		var h: number = fcolor1.h / 360;
-		var s: number = fcolor1.s / 100;
-		var v: number = fcolor1.v / 100;
-		//var h = 145 / 360; // green
-		var reduced: number = (cents / 1200) % 1;
-		if (reduced < 0)
-			reduced += 1;
-		h = (reduced + h) % 1;
-		
-		v = pressed ? v - (v / 2) : v;
-		
-		//setup text color
-		current_text_color = HSVtoRGB(h, s, v);
-		return current_text_color.toCssRgb();
-		
-	} else {
-		let colorStr: string;
-		if (typeof(notUndefined(settings.keycolors)[global_pressed_interval]) === "undefined")
-			colorStr = "#EDEDE4";
-		else
-			colorStr = notUndefined(settings.keycolors)[global_pressed_interval];
-		
-		//convert color name to hex
-		current_text_color = Rgb8Color.fromCssHex(nameToHex(colorStr));
-		
-		//convert the hex to rgb
-		let {red, green, blue} = current_text_color;
-		
-		//darken for pressed key
-		if (pressed) {
-			red -= 90;
-			green -= 90;
-		}
-		
-		return new Rgb8Color(red, green, blue).toCssRgb();
-	}
-}
-
 function roundTowardZero(val: number): number {
 	if (val < 0)
 		return Math.ceil(val);
@@ -1184,39 +1420,6 @@ function getHexCoordsAt(coords: Point): Point {
 	return closestHex;
 }
 
-function HSVtoRGB(h: number, s: number, v: number): Rgb8Color {
-	var r: number, g: number, b: number, i: number, f: number, p: number, q: number, t: number;
-	i = Math.floor(h * 6);
-	f = h * 6 - i;
-	p = v * (1 - s);
-	q = v * (1 - f * s);
-	t = v * (1 - (1 - f) * s);
-	switch (i % 6) {
-		case 0:
-			r = v; g = t; b = p;
-			break;
-		case 1:
-			r = q; g = v; b = p;
-			break;
-		case 2:
-			r = p; g = v; b = t;
-			break;
-		case 3:
-			r = p; g = q; b = v;
-			break;
-		case 4:
-			r = t; g = p; b = v;
-			break;
-		case 5:
-			r = v; g = p; b = q;
-			break;
-		default:
-			throw new Error("Unreachable");
-	}
-	
-	return new Rgb8Color(Math.floor(r * 255), Math.floor(g * 255), Math.floor(b * 255));
-}
-
 window.addEventListener("load", init, false);
 
 function init(): void {
@@ -1265,205 +1468,6 @@ function tempAlert(msg: string, duration: number): void {
 		el.remove();
 	}, duration);
 	document.body.appendChild(el);
-}
-
-
-function nameToHex(colour: string): string {
-	var colours: {[index:string]: string} = {
-		"aliceblue": "#f0f8ff",
-		"antiquewhite": "#faebd7",
-		"aqua": "#00ffff",
-		"aquamarine": "#7fffd4",
-		"azure": "#f0ffff",
-		"beige": "#f5f5dc",
-		"bisque": "#ffe4c4",
-		"black": "#000000",
-		"blanchedalmond": "#ffebcd",
-		"blue": "#0000ff",
-		"blueviolet": "#8a2be2",
-		"brown": "#a52a2a",
-		"burlywood": "#deb887",
-		"cadetblue": "#5f9ea0",
-		"chartreuse": "#7fff00",
-		"chocolate": "#d2691e",
-		"coral": "#ff7f50",
-		"cornflowerblue": "#6495ed",
-		"cornsilk": "#fff8dc",
-		"crimson": "#dc143c",
-		"cyan": "#00ffff",
-		"darkblue": "#00008b",
-		"darkcyan": "#008b8b",
-		"darkgoldenrod": "#b8860b",
-		"darkgray": "#a9a9a9",
-		"darkgreen": "#006400",
-		"darkkhaki": "#bdb76b",
-		"darkmagenta": "#8b008b",
-		"darkolivegreen": "#556b2f",
-		"darkorange": "#ff8c00",
-		"darkorchid": "#9932cc",
-		"darkred": "#8b0000",
-		"darksalmon": "#e9967a",
-		"darkseagreen": "#8fbc8f",
-		"darkslateblue": "#483d8b",
-		"darkslategray": "#2f4f4f",
-		"darkturquoise": "#00ced1",
-		"darkviolet": "#9400d3",
-		"deeppink": "#ff1493",
-		"deepskyblue": "#00bfff",
-		"dimgray": "#696969",
-		"dodgerblue": "#1e90ff",
-		"firebrick": "#b22222",
-		"floralwhite": "#fffaf0",
-		"forestgreen": "#228b22",
-		"fuchsia": "#ff00ff",
-		"gainsboro": "#dcdcdc",
-		"ghostwhite": "#f8f8ff",
-		"gold": "#ffd700",
-		"goldenrod": "#daa520",
-		"gray": "#808080",
-		"green": "#008000",
-		"greenyellow": "#adff2f",
-		"honeydew": "#f0fff0",
-		"hotpink": "#ff69b4",
-		"indianred": "#cd5c5c",
-		"indigo": "#4b0082",
-		"ivory": "#fffff0",
-		"khaki": "#f0e68c",
-		"lavender": "#e6e6fa",
-		"lavenderblush": "#fff0f5",
-		"lawngreen": "#7cfc00",
-		"lemonchiffon": "#fffacd",
-		"lightblue": "#add8e6",
-		"lightcoral": "#f08080",
-		"lightcyan": "#e0ffff",
-		"lightgoldenrodyellow": "#fafad2",
-		"lightgrey": "#d3d3d3",
-		"lightgreen": "#90ee90",
-		"lightpink": "#ffb6c1",
-		"lightsalmon": "#ffa07a",
-		"lightseagreen": "#20b2aa",
-		"lightskyblue": "#87cefa",
-		"lightslategray": "#778899",
-		"lightsteelblue": "#b0c4de",
-		"lightyellow": "#ffffe0",
-		"lime": "#00ff00",
-		"limegreen": "#32cd32",
-		"linen": "#faf0e6",
-		"magenta": "#ff00ff",
-		"maroon": "#800000",
-		"mediumaquamarine": "#66cdaa",
-		"mediumblue": "#0000cd",
-		"mediumorchid": "#ba55d3",
-		"mediumpurple": "#9370d8",
-		"mediumseagreen": "#3cb371",
-		"mediumslateblue": "#7b68ee",
-		"mediumspringgreen": "#00fa9a",
-		"mediumturquoise": "#48d1cc",
-		"mediumvioletred": "#c71585",
-		"midnightblue": "#191970",
-		"mintcream": "#f5fffa",
-		"mistyrose": "#ffe4e1",
-		"moccasin": "#ffe4b5",
-		"navajowhite": "#ffdead",
-		"navy": "#000080",
-		"oldlace": "#fdf5e6",
-		"olive": "#808000",
-		"olivedrab": "#6b8e23",
-		"orange": "#ffa500",
-		"orangered": "#ff4500",
-		"orchid": "#da70d6",
-		"palegoldenrod": "#eee8aa",
-		"palegreen": "#98fb98",
-		"paleturquoise": "#afeeee",
-		"palevioletred": "#d87093",
-		"papayawhip": "#ffefd5",
-		"peachpuff": "#ffdab9",
-		"peru": "#cd853f",
-		"pink": "#ffc0cb",
-		"plum": "#dda0dd",
-		"powderblue": "#b0e0e6",
-		"purple": "#800080",
-		"red": "#ff0000",
-		"rosybrown": "#bc8f8f",
-		"royalblue": "#4169e1",
-		"saddlebrown": "#8b4513",
-		"salmon": "#fa8072",
-		"sandybrown": "#f4a460",
-		"seagreen": "#2e8b57",
-		"seashell": "#fff5ee",
-		"sienna": "#a0522d",
-		"silver": "#c0c0c0",
-		"skyblue": "#87ceeb",
-		"slateblue": "#6a5acd",
-		"slategray": "#708090",
-		"snow": "#fffafa",
-		"springgreen": "#00ff7f",
-		"steelblue": "#4682b4",
-		"tan": "#d2b48c",
-		"teal": "#008080",
-		"thistle": "#d8bfd8",
-		"tomato": "#ff6347",
-		"turquoise": "#40e0d0",
-		"violet": "#ee82ee",
-		"wheat": "#f5deb3",
-		"white": "#ffffff",
-		"whitesmoke": "#f5f5f5",
-		"yellow": "#ffff00",
-		"yellowgreen": "#9acd32",
-	};
-
-	if (typeof colours[colour.toLowerCase()] != "undefined")
-		return colours[colour.toLowerCase()];
-	else if (colour.indexOf("#") == 0)
-		return colour;
-	else if (colour.length == 6 && colour.indexOf("#") == -1)
-		return "#" + colour;
-	
-	return "#EDEDE4"; //default button color!
-}
-
-function rgb2hsv(rgb: Rgb8Color): {h:number, s:number, v:number} {
-	var rr: number, gg: number, bb: number,
-		r: number = rgb.red / 255,
-		g: number = rgb.green / 255,
-		b: number = rgb.blue / 255,
-		h: number, s: number,
-		v: number = Math.max(r, g, b),
-		diff: number = v - Math.min(r, g, b),
-		diffc = function(c: number): number {
-			return (v - c) / 6 / diff + 1 / 2;
-		};
-	
-	if (diff == 0)
-		h = s = 0;
-	else {
-		s = diff / v;
-		rr = diffc(r);
-		gg = diffc(g);
-		bb = diffc(b);
-		
-		if (r === v)
-			h = bb - gg;
-		else if (g === v)
-			h = (1 / 3) + rr - bb;
-		else if (b === v)
-			h = (2 / 3) + gg - rr;
-		else
-			throw new Error("Unreachable");
-		if (h < 0)
-			h += 1;
-		else if (h > 1)
-			h -= 1;
-	}
-	return {
-		h: Math.round(h * 360),
-		s: Math.round(s * 100),
-		v: Math.round(v * 100),
-	};
-}
-
-function getContrastYIQ({red, green, blue}: Rgb8Color): string {
-	return (red * 299 + green * 587 + blue * 114) / 1000 >= 128 ? "black" : "white";
 }
 
 
