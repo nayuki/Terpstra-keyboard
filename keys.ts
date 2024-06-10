21736202de4edfb22fad936a1aa064d3f0702640
Project Nayuki <me@nayuki.io>
2024-06-10 02:17:03 +0000

    Simplified a function by leveraging a nearly identical one.

--- keys.ts
+++ keys.ts
@@ -1168,35 +1168,8 @@ function rgb(r: number, g: number, b: number): string {
 }
 
 function HSVtoRGB(h: number, s: number, v: number): string {
-	var r: number, g: number, b: number, i: number, f: number, p: number, q: number, t: number;
-	i = Math.floor(h * 6);
-	f = h * 6 - i;
-	p = v * (1 - s);
-	q = v * (1 - f * s);
-	t = v * (1 - (1 - f) * s);
-	switch (i % 6) {
-		case 0:
-			r = v; g = t; b = p;
-			break;
-		case 1:
-			r = q; g = v; b = p;
-			break;
-		case 2:
-			r = p; g = v; b = t;
-			break;
-		case 3:
-			r = p; g = q; b = v;
-			break;
-		case 4:
-			r = t; g = p; b = v;
-			break;
-		case 5:
-			r = v; g = p; b = q;
-			break;
-		default:
-			throw new Error("Unreachable");
-	}
-	return rgb(Math.floor(r * 255), Math.floor(g * 255), Math.floor(b * 255));
+	const {red, green, blue} = HSVtoRGB2(h, s, v);
+	return rgb(red, green, blue);
 }
 
 function HSVtoRGB2(h: number, s: number, v: number): {red:number, green:number, blue:number} {
